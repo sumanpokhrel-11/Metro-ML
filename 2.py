f61bd47d61080c4054ea885f4345c43387cab981
sumanpokhrel-11 <11pksuman@gmail.com>
2024-05-21 08:36:43 +0545

    updated

--- 2.py
+++ 2.py
@@ -1,1 +1,6 @@
 print("creating local branch")
+a = 'sum'
+a.isdigit() # checks if the variable is digit or not and returns boolean values
+import random
+a = [1, 2, 3]
+b = random.Random.choices
---
+++ rock_paper_scissor.py
@@ -0,0 +1,27 @@
+import random
+choices = ['r', 'p', 's']
+ATTEMPT = 1
+def play():
+    global ATTEMPT
+    ATTEMPT
+    try:
+        user_guess = str(input("Enter ROCK > [R] OR PAPER > [P] OR SCISSOR > [S]")).lower()
+    except Exception as ex:
+        print("Wrong Input")
+        user_guess = str(input("Enter ROCK > [R] OR PAPER > [P] OR SCISSOR > [S]")).lower()
+    bot = random.choice(choices)
+    bot_guess = bot[0]
+
+    if (bot_guess =="r" and user_guess =="p") or (bot_guess =="p" and user_guess =="s") or(bot_guess =="s" and user_guess=="r"):
+        print("You Won")
+        print("---------------------------------------------------------")
+        print(f"You took {ATTEMPT} attempts")
+        winner = True
+    else:
+        ATTEMPT +=1
+        print("You lose")
+        winner = False
+    return winner
+
+while not play():
+    continue
