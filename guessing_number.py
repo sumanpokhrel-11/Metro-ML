4026ca7e95aaccee5617ab5825d6a931bc6f7e70
sumanpokhrel-11 <11pksuman@gmail.com>
2024-05-21 07:45:11 +0545

    guessing number game using random module

--- guessing_number.py
+++ guessing_number.py
@@ -1,19 +1,24 @@
 import random
 
-win = False
-num_to_guess = random.randint(1,10)
-user_guess = int(input("Enter the Number between 1 to 10: "))
+def guess_number():
+    num_to_guess = random.randint(1,10)
+    user_guess = int(input("Enter the Number between 1 to 10: "))
 
 
-while not win:
     if num_to_guess == user_guess:
+        winner = True
         print("You Won!!")
-        win = True
+        
+        
     else:
-        win = False
+        winner = False
         if num_to_guess<user_guess:
             print("Guess Lower")
         elif num_to_guess>user_guess:
             print("Guess Higher")
         else:
             print("Enter number only")
+    return winner
+
+while not guess_number():
+    guess_number()
