fb7a639328b98660eb009e3e5d245cb5f3852447
sumanpokhrel-11 <11pksuman@gmail.com>
2024-05-21 08:11:03 +0545

    try except is added in the game

--- guessing_number.py
+++ guessing_number.py
@@ -1,16 +1,22 @@
 import random
-
-def guess_number():
-    num_to_guess = random.randint(1,10)
-    user_guess = int(input("Enter the Number between 1 to 10: "))
-
-
+ATTEMPT = 1
+def guess_number(num_to_guess):
+    try:
+        user_guess = int(input("Enter the Number between 1 to 10: "))
+    except Exception as ex:
+        print("Enter digits only")
+        user_guess = int(input("Enter the Number between 1 to 10: "))
+    global ATTEMPT
+    ATTEMPT
     if num_to_guess == user_guess:
         winner = True
         print("You Won!!")
+        print(f"YOU TOOK {ATTEMPT} ATTEMPTS TO WIN")
         
         
     else:
+        
+        ATTEMPT += 1
         winner = False
         if num_to_guess<user_guess:
             print("Guess Lower")
@@ -20,5 +26,9 @@ def guess_number():
             print("Enter number only")
     return winner
 
-while not guess_number():
-    guess_number()
+num_to_guess = random.randint(1,10)
+while not guess_number(num_to_guess):
+    continue
+    
+    
+    
